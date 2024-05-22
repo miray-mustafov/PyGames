6f8e48b366c69396dd10c7b49b0542b236af9da2
miray-mustafov <mustafovmiray@gmail.com>
2024-05-23 00:10:04 +0300

    gitignore, requirements

--- Snake/snake.py
+++ Snake/snake.py
@@ -2,13 +2,6 @@ import random
 import time
 import pygame as py
 
-class Retard:
-    def __init__(self, papagal):
-        self.papagal = papagal
-
-    def get_papagal(self, info):
-        return f"{info} etremo {self.papagal}"
-
 py.init()
 width, height = 400, 400
 game_screen = py.display.set_mode((width, height))
@@ -20,7 +13,7 @@ delta_x, delta_y = 0, 0
 snake_speed = 10
 clock = py.time.Clock()
 food_x, food_y = random.randint(
-0, width) // 10 * 10, random.randint(0, height) // 10 * 10
+    0, width) // 10 * 10, random.randint(0, height) // 10 * 10
 body_list = [(x, y)]
 game_OVER = False
 font = py.font.SysFont('bahnschrift', bold=True, size=25)
@@ -41,18 +34,6 @@ def snake():
     if food_x == x and food_y == y:
         while (food_x, food_y) in body_list:
             food_x, food_y = random.randint(
-
-                0, width)//10*10, random.randint(0, height)//10*10
-        if len(body_list) % 3 == 0:
-            snake_speed +=2
-    else:
-        del body_list[0]
-    
-    
-
-    game_screen.fill((0, 0, 0))
-    score = font.render('Score: '+ str(len(body_list)), True,(255,255,0))
-    game_screen.blit(score, [0,0])
                 0, width) // 10 * 10, random.randint(0, height) // 10 * 10
         if len(body_list) % 3 == 0:
             snake_speed += 2
@@ -71,10 +52,9 @@ def snake():
 
 
 while True:
-    if game_OVER == True:
+    if game_OVER:
         game_screen.fill((0, 0, 0))
         gover_msg = font.render("GAME OVER!", True, (255, 255, 255))
-
         game_screen.blit(gover_msg, dest=[width // 3, height // 3])
 
         game_screen.blit(font.render("Final score: " + str(len(body_list)), True, (255, 255, 0))
@@ -92,19 +72,19 @@ while True:
             quit()
         if ev.type == py.KEYDOWN:
             if ev.key == py.K_UP:
-                if (delta_y != +10):
+                if delta_y != +10:
                     delta_y = -10
                 delta_x = 0
             elif ev.key == py.K_DOWN:
-                if (delta_y != -10):
+                if delta_y != -10:
                     delta_y = +10
                 delta_x = 0
             elif ev.key == py.K_RIGHT:
-                if (delta_x != -10):
+                if delta_x != -10:
                     delta_x = +10
                 delta_y = 0
             elif ev.key == py.K_LEFT:
-                if (delta_x != +10):
+                if delta_x != +10:
                     delta_x = -10
                 delta_y = 0
             else:
